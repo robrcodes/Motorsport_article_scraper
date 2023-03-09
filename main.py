67c532f9d4dbe83690c50650773cd595f627eb85
Rob <robjrenwick@gmail.com>
2023-03-09 16:27:16 +1000

    Added published date to front of  text file name
    Scrapes the article published date text and converts to datetime object, adding it as text in a different format to start of file written. Also add extracted date to bottom of text file.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import requests
+from datetime import datetime
+
+# get today's date to add to bottome of article text file
+today_date = datetime.today()
 
 # required: bs4, requests, lxml
 
@@ -17,6 +21,7 @@ main_box = soup.find('div', class_='post-block-first')
 # identify the featured article title
 mb_title = main_box.find('h4', class_='entryTitle')
 
+
 # identify the featured article page link
 page_link = mb_title.find('a', class_='animate').get('href')
 
@@ -31,12 +36,28 @@ main_content = feature_soup.find('div', class_='entryContent')
 # Use CSS selector to specify only the required content, the article text, goes into the list
 feature_article = main_content.select('div.entryContent > p')
 
+# get the article date meta 
+published_date = feature_soup.find('p', class_='date meta')
+
+# current format date text is displayed on page
+format = '%A %d %B %Y %I:%M%p'
+
+# clean text date to simplify conversion to date object
+mydate = published_date.get_text().replace('th', '').replace(',','').replace('- ','')
+
+# create date object from text date
+dateobj = datetime.strptime(mydate, format)
+
 # write the list of the article content into a text file using a loop to add each list element
-with open(f'SC-{mb_title.get_text()}.txt', 'w') as file:
+with open(f'{dateobj.date()}-{dateobj.time()}-{mb_title.get_text()[0:20]}.txt', 'w') as file:
     file.write(mb_title.get_text().upper())
     for para in feature_article:
         # write each line into file
         file.write("%s\n\n" % para.get_text())
 
+    # add today's date at bottom of article text file
+    file.write('Extracted: ')
+    file.write("%s\n\n" % today_date)
+    
     # close the file once complete
     file.close()
